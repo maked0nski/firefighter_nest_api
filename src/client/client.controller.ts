1c2c6c774cf6b4339cdea8dcbd65666d71300a58
maked0nski <maked0nskihtc@gmail.com>
2022-07-05 12:08:14 +0300

    controllers and services for client table

--- src/client/client.controller.ts
+++ src/client/client.controller.ts
@@ -1,4 +1,88 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, UseGuards} from '@nestjs/common';
+import {ApiBody, ApiForbiddenResponse, ApiNotFoundResponse, ApiOperation, ApiParam, ApiTags} from "@nestjs/swagger";
 
+import {CreateClientDto, UpdateClientDto} from "./dto";
+import {ClientService} from "./client.service";
+import {CustomOkResponse} from "../utils";
+import {Exception} from "../exceptions";
+import {AtGuard} from "../core/guards";
+import {
+    SWAGGER_CLIENT_BODY_EXAMPLE,
+    SWAGGER_CLIENT_EXAMPLE, SWAGGER_CLIENT_LIST_EXAMPLE
+} from "../utils/example";
+
+
+@ApiTags('Фірми клієнти')
 @Controller('client')
-export class ClientController {}
+@UseGuards(AtGuard)
+export class ClientController {
+
+    constructor(private clientService:ClientService) {
+    }
+
+
+    @ApiOperation({summary: 'Create client firm'})
+    @ApiForbiddenResponse({description: Exception.FORBIDDEN})
+    @CustomOkResponse({status: HttpStatus.CREATED, exampleData: SWAGGER_CLIENT_EXAMPLE})
+    @ApiBody({
+        description: "Example body",
+        schema: {
+            example: SWAGGER_CLIENT_BODY_EXAMPLE
+        },
+        required: true
+    })
+    @HttpCode(HttpStatus.CREATED)
+    @Post()
+    create(@Body() dto: CreateClientDto): Promise<CreateClientDto> {
+        return this.clientService.create(dto)
+    };
+
+    @ApiOperation({summary: 'Get all client firms'})
+    @ApiForbiddenResponse({description: Exception.FORBIDDEN})
+    @CustomOkResponse({status: HttpStatus.OK, exampleData: SWAGGER_CLIENT_LIST_EXAMPLE})
+    @ApiNotFoundResponse({description: Exception.FIRM_CLIENT_NOT_FOUND})
+    @HttpCode(HttpStatus.OK)
+    @Get()
+    getAll(): Promise<CreateClientDto[]> {
+        return this.clientService.getAll();
+    }
+
+    @ApiOperation({summary: 'Get client firms by id'})
+    @ApiForbiddenResponse({description: Exception.FORBIDDEN})
+    @CustomOkResponse({status: HttpStatus.OK, exampleData: SWAGGER_CLIENT_EXAMPLE})
+    @ApiNotFoundResponse({description: Exception.FIRM_CLIENT_NOT_FOUND})
+    @HttpCode(HttpStatus.OK)
+    @Get('id')
+    getById(@Param('id') id: string): Promise<CreateClientDto> {
+        return this.clientService.getById(Number(id));
+    }
+
+    @ApiOperation({summary: 'Update client firms by id'})
+    @CustomOkResponse({status: HttpStatus.CREATED, exampleData: SWAGGER_CLIENT_EXAMPLE})
+    @ApiBody({
+        description: "Example body",
+        schema: {
+            example: SWAGGER_CLIENT_BODY_EXAMPLE
+        },
+        required: false
+    })
+    @ApiParam({name: "id", description: 'Client firms id', schema: {example: 1}})
+    @ApiForbiddenResponse({description: Exception.FORBIDDEN})
+    @ApiNotFoundResponse({description: Exception.FIRM_CLIENT_NOT_FOUND})
+    @HttpCode(HttpStatus.CREATED)
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() dto: UpdateClientDto): Promise<CreateClientDto> {
+        return this.clientService.update(Number(id), dto);
+    }
+
+    @ApiOperation({summary: 'Delete client firms by id'})
+    @ApiNotFoundResponse({description: Exception.FIRM_CLIENT_NOT_FOUND})
+    @ApiParam({name: "id", description: 'Client firms id', schema: {example: 1}})
+    @HttpCode(HttpStatus.NO_CONTENT)
+    @Delete(':id')
+    delete(@Param('id') id: string): Promise<CreateClientDto> {
+        return this.clientService.delete(Number(id));
+    }
+
+
+}
--- src/client/client.service.ts
+++ src/client/client.service.ts
@@ -1,4 +1,104 @@
-import { Injectable } from '@nestjs/common';
+import {ForbiddenException, HttpException, HttpStatus, Injectable, NotFoundException} from '@nestjs/common';
+import {CreateClientDto, UpdateClientDto} from "./dto";
+import {PrismaService} from "../core/prisma.service";
+import {PrismaClientKnownRequestError} from "@prisma/client/runtime";
+import {Exception} from "../exceptions";
 
 @Injectable()
-export class ClientService {}
+export class ClientService {
+
+    constructor(private prismaService: PrismaService) {
+    }
+
+    create(dto: CreateClientDto): Promise<CreateClientDto> {
+        return Promise
+            .resolve(this.prismaService.client
+                .create({data: dto}))
+            .catch((error) => {
+                if (error instanceof PrismaClientKnownRequestError) {
+                    if (error.code === 'P2002') {
+                        throw new ForbiddenException(Exception.FORBIDDEN);
+                    }
+                    if (error.code === 'P2025') {
+                        throw new NotFoundException(Exception.FIRM_CLIENT_NOT_FOUND)
+                    }
+                }
+                throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+            })
+    }
+
+    getAll(): Promise<CreateClientDto[]> {
+        return Promise
+            .resolve(this.prismaService.client
+                .findMany())
+            .catch((error) => {
+                if (error instanceof PrismaClientKnownRequestError) {
+                    if (error.code === 'P2002') {
+                        throw new ForbiddenException(Exception.FORBIDDEN);
+                    }
+                    if (error.code === 'P2025') {
+                        throw new NotFoundException(Exception.FIRM_CLIENT_NOT_FOUND)
+                    }
+                }
+                throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+            })
+    }
+
+    getById(id: number): Promise<CreateClientDto> {
+        return Promise
+            .resolve(this.prismaService.client
+                .findFirstOrThrow({
+                    where: {id}
+                }))
+            .catch((error) => {
+                if (error instanceof PrismaClientKnownRequestError) {
+                    if (error.code === 'P2002') {
+                        throw new ForbiddenException(Exception.FORBIDDEN);
+                    }
+                    if (error.code === 'P2025') {
+                        throw new NotFoundException(Exception.FIRM_CLIENT_NOT_FOUND)
+                    }
+                }
+                throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+            })
+    }
+
+    update(id: number, dto: UpdateClientDto): Promise<CreateClientDto> {
+        return Promise
+            .resolve(this.prismaService.client
+                .update({
+                    where: {id},
+                    data: dto
+                }))
+            .catch((error) => {
+                if (error instanceof PrismaClientKnownRequestError) {
+                    if (error.code === 'P2002') {
+                        throw new ForbiddenException(Exception.FORBIDDEN);
+                    }
+                    if (error.code === 'P2025') {
+                        throw new NotFoundException(Exception.FIRM_CLIENT_NOT_FOUND)
+                    }
+                }
+                throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+            })
+    }
+
+    delete(id: number): Promise<CreateClientDto> {
+        return Promise
+            .resolve(this.prismaService.client
+                .delete({
+                    where: {id}
+                }))
+            .catch((error) => {
+                if (error instanceof PrismaClientKnownRequestError) {
+                    if (error.code === 'P2002') {
+                        throw new ForbiddenException(Exception.FORBIDDEN);
+                    }
+                    if (error.code === 'P2025') {
+                        throw new NotFoundException(Exception.FIRM_CLIENT_NOT_FOUND)
+                    }
+                }
+                throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+            })
+    }
+}
